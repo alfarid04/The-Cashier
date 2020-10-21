1c432a6f0ecbe2d621e27d3bd100fbd6f3904871
alfarid04 <61859331+alfarid04@users.noreply.github.com>
2020-10-22 00:07:04 +0700

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,30 @@
+﻿# The Cashier
+Aplikasi ini mencakup fungsi perhitungan yaitu penjumlahan dan perkalian yang digunakan untuk menghitung jumlah barang/jasa yang akan dibeli oleh pembeli atau pelanggan
+
+# Scope & Functionalities
+~user dapat mengetikan/memasukkan kata(huruf/angka)
+
+~user dapat memilih barang/jasa yang akan dibeli
+
+~user dapat menyentuh tombol Tambahkan
+
+~user dapat melihat data list yang telah ditambahkan sebelumnya
+# How Does it Works?
+Diawali dengan method MainWindow pada class MainWindow.xaml.cs, kita mendeklarasikan
+```csharp
+ public MainWindow()
+        {
+            InitializeComponent();
+            Calculator = new Calculator();
+            Listbox.ItemsSource = Calculator.getListItem();
+        }
+```
+logika perhitungan terdapat pada class Item.cs Cara kerjanya yaitu menjumlahkan semua price dengan quantitynya.
+```csharp
+public double getSubtotal()
+        {
+            subtotal = price * quantity;
+            return subtotal;
+        }
+```
+# Terima Kasih
---
+++ The Cashier/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace The_Cashier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Calculator Calculator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Calculator = new Calculator();
+            Listbox.ItemsSource = Calculator.getListItem();
+        }
+            private void AddButton_Click(object sender, RoutedEventArgs e)
+            {
+                string title = ItemNameBox.Text;
+                int quantity = int.Parse(QuantityBox.Text);
+                string type = TypeBox.Text;
+                double price = double.Parse(PriceBox.Text);
+
+
+
+                Item item = new Item(new Random().Next(), title, quantity, price, price, type);
+                Calculator.addItem(item);
+                double total = Calculator.getTotal();
+
+                TotalLabel.Content = string.Format("Rp. {0}", total);
+
+                Listbox.Items.Refresh();
+            }
+        }
+    }
